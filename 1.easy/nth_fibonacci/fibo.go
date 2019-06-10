43dec121541fa94327ff11052e76a0b104ab04fb
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-10 13:55:34 +0700

    finish fibo loop

--- 1.easy/nth_fibonacci/fibo.go
+++ 1.easy/nth_fibonacci/fibo.go
@@ -1,5 +1,17 @@
 package fibo
 
 func GetNthFib1(n int) int {
-	return 1
+	result := 0
+	nums := []int{0, 1}
+
+	if n <= 2 {
+		return nums[n-1]
+	}
+
+	for i := 2; i < n; i++ {
+		result = nums[i-2] + nums[i-1]
+		nums = append(nums, result)
+	}
+
+	return result
 }
