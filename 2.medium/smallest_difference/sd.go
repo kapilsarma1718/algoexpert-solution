90c27d3bb855ee9cf871c5ebe3ae4f7c04af63eb
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-12 13:43:28 +0700

    smallest difference1 done

--- 2.medium/smallest_difference/sd.go
+++ 2.medium/smallest_difference/sd.go
@@ -1,7 +1,31 @@
 package sd
 
 func SmallestDifference1(arr1, arr2 []int) []int {
-	return []int{}
+	store := make([]int, 2)
+	diff := 9999
+	tmpDiff := 0
+
+	for i := 0; i < len(arr1); i++ {
+		num1 := arr1[i]
+		for j := 0; j < len(arr2); j++ {
+			num2 := arr2[j]
+
+			if num1 > num2 {
+				tmpDiff = num1 - num2
+			} else if num2 > num1 {
+				tmpDiff = num2 - num1
+			}
+
+			if tmpDiff < diff {
+				diff = tmpDiff
+				store[0] = num1
+				store[1] = num2
+			}
+		}
+	}
+
+	return store
+
 }
 
 func SmallestDifference2(arr1, arr2 []int) []int {
