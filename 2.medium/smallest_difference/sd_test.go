993bd71ab138a584a715da52d8233175932b99b3
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-12 13:35:26 +0700

    initial testing

---
+++ 2.medium/smallest_difference/sd_test.go
@@ -0,0 +1,39 @@
+package sd_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	td "algoexpert-solution/2.medium/smallest_difference"
+)
+
+type testCase struct {
+	arg1 []int
+	arg2 []int
+	want []int
+}
+
+func TestSmallestDifference(t *testing.T) {
+	caseData := []testCase{
+		{
+			[]int{-1, 5, 10, 20, 3},
+			[]int{26, 134, 135, 15, 17},
+			[]int{20, 17},
+		},
+		{
+			[]int{-1, 5, 10, 20, 28, 3},
+			[]int{26, 134, 135, 15, 17},
+			[]int{28, 26},
+		},
+	}
+
+	for _, tc := range caseData {
+		t.Run(fmt.Sprintf("SmallestDifference1(%v, %v)", tc.arg1, tc.arg2), func(t *testing.T) {
+			result := td.SmallestDifference1(tc.arg1, tc.arg2)
+			if !reflect.DeepEqual(result, tc.want) {
+				t.Errorf("SmallestDifference1(%v, %v) = %v, want : %v", tc.arg1, tc.arg2, result, tc.want)
+			}
+		})
+	}
+}
