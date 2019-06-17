f620f287d51eb9b6abccf3c9e60805d798a89cdd
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-17 12:05:56 +0700

    finish powerset

--- 2.medium/powerset/main.go
+++ 2.medium/powerset/main.go
@@ -3,7 +3,28 @@ package main
 import "fmt"
 
 func Powerset(array []int) [][]int {
-	return [][]int{}
+	sets := [][]int{
+		[]int{},
+	}
+
+	for i := 0; i < len(array); i++ {
+		lenSets := len(sets)
+		for j := 0; j < lenSets; j++ {
+
+			// copy newSubSet value with the sets
+			newSubSet := []int{}
+			newSubSet = append(newSubSet, sets[j]...)
+
+			// insert number to newSubSet
+			newSubSet = append(newSubSet, array[i])
+
+			// insert newSubSet to the sets
+			sets = append(sets, newSubSet)
+
+		}
+	}
+
+	return sets
 }
 
 func main() {
--- 2.medium/powerset/main_test.go
+++ 2.medium/powerset/main_test.go
@@ -28,7 +28,7 @@ func TestPowerset(t *testing.T) {
 				[]int{2},
 				[]int{1, 2},
 				[]int{3},
-				[]int{1, 2},
+				[]int{1, 3},
 				[]int{2, 3},
 				[]int{1, 2, 3},
 			},
