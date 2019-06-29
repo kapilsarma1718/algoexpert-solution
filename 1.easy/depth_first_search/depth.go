9954ea7bca9123cf8d0feca0a40352df726c9ab7
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-29 21:03:58 +0700

    breadth first search readme

--- 1.easy/depth_first_search/depth.go
+++ 1.easy/depth_first_search/depth.go
@@ -30,6 +30,9 @@ func (n *Node) AddChildren(names ...string) *Node {
 
 func (n *Node) DepthFirstSearch(array []string) []string {
 	array = append(array, n.Name)
+	if len(n.Children) == 0 {
+		return array
+	}
 	for _, child := range n.Children {
 		array = child.DepthFirstSearch(array)
 	}
---
+++ 2.medium/breadth_first_search/readme.md
@@ -0,0 +1,4 @@
+breadth first search
+
+Problem:
+Print semua elemen dalam tree menggunakan teknik breadth first search
