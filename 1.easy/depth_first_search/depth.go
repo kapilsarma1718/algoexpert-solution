be4f52809501b38c35dd170ff95ac147310ddaa6
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-06-29 12:56:49 +0700

    finish create depth first search main

---
+++ 1.easy/depth_first_search/depth.go
@@ -0,0 +1,13 @@
+package depth
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func NewNode(name string) *Node {
+	return &Node{
+		Name:     name,
+		Children: []*Node{},
+	}
+}
---
+++ 1.easy/depth_first_search/readme.md
@@ -0,0 +1,4 @@
+depth first search
+
+Problem:
+Cetak semua elemen dalam tree menggunakan depth first search
