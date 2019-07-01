88f3a3faaed8ee5ba5ee5a70969d8d246847074f
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-07-01 16:25:50 +0700

    finish bubble sort

--- 1.easy/bubble_sort/bubble.go
+++ 1.easy/bubble_sort/bubble.go
@@ -1,5 +1,15 @@
 package bs
 
 func BubbleSort(array []int) []int {
-	return []int{}
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			num1 := array[j]
+			num2 := array[j+1]
+
+			if num1 > num2 {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
 }
