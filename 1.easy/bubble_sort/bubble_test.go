83a60f3bcaa83e2f99555f153b630495769a0571
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-07-01 16:07:12 +0700

    finish bubble sort test

---
+++ 1.easy/bubble_sort/bubble_test.go
@@ -0,0 +1,43 @@
+package bs_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	tt "algoexpert-solution/1.easy/bubble_sort"
+)
+
+type TestCase struct {
+	arg  []int
+	want []int
+}
+
+func TestBinarySearch(t *testing.T) {
+	caseData := []TestCase{
+		{
+			[]int{},
+			[]int{},
+		},
+		{
+			[]int{1, 3, 2},
+			[]int{1, 2, 3},
+		},
+		{
+			[]int{-10, -7, -7, -6, -6, -5, -5, -4, -4, -4, -2, -1, 1, 3, 5, 5, 6, 8, 8, 10},
+			[]int{-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8},
+		},
+	}
+
+	for _, tc := range caseData {
+		t.Run(fmt.Sprintf("BubbleSort(%v)", tc.arg), func(t *testing.T) {
+			result := tt.BubbleSort(tc.arg)
+
+			if !reflect.DeepEqual(result, tc.want) {
+				t.Errorf("BubbleSort(%v) = %v, want : %v", tc.arg, result, tc.want)
+			}
+
+		})
+	}
+
+}
