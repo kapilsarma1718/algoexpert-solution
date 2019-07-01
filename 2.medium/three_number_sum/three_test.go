7219619d26532cb8fa21000cff9249389e716f94
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-07-01 20:28:40 +0700

    finish three number sum test

---
+++ 2.medium/three_number_sum/three_test.go
@@ -0,0 +1,46 @@
+package tns_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	tt "algoexpert-solution/2.medium/three_number_sum"
+)
+
+type testCase struct {
+	arg1 []int
+	arg2 int
+	want [][]int
+}
+
+func TestThreeNumberSum(t *testing.T) {
+	caseData := []testCase{
+		{
+			[]int{},
+			0,
+			[][]int{},
+		},
+		{
+			[]int{12, 3, 1, 2, -6, 5, -8, 6},
+			0,
+			[][]int{
+				[]int{-8, 2, 6},
+				[]int{-8, 3, 5},
+				[]int{-6, 1, 5},
+			},
+		},
+	}
+
+	for _, tc := range caseData {
+		t.Run(fmt.Sprintf("ThreeNumberSum(%v, %v)", tc.arg1, tc.arg2), func(t *testing.T) {
+			result := tt.ThreeNumberSum(tc.arg1, tc.arg2)
+
+			if !reflect.DeepEqual(result, tc.want) {
+
+				t.Errorf("ThreeNumberSum(%v, %v) = %v, want : %v", tc.arg1, tc.arg2, result, tc.want)
+			}
+
+		})
+	}
+}
